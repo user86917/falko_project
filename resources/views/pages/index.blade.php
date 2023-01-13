7b5040acb5990f1f4d9580ca79422160877495fa
falko Sam-Sin <86917@glr.nl>
2023-01-13 13:43:32 +0100

    falko css

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
-@extends('layout'),
-@section('content'),
+@extends('layout')
+@section('content')
     
+{{-- @push('css')
+    <link rel="stylesheet" href="path to your css">
+@endpush --}}
 
 @include('components.blogform')
 
